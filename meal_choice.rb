df703d68b7db40a23b8a09a577a20837f2b96cf3
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-03-11 00:15:58 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,3 +1,3 @@
 def meal_choice(meal = "meat")# Define a method meal_choice that returns the meal_choice that was
-  return meat# passed into it and defaults to meat.
+  return meal# passed into it and defaults to meat.
 end
